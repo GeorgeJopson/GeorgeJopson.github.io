75bb792eddc160dad6380a0e3f07f413726760fa
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-06 22:46:17 +0000

    Make Bio section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './fonts.css';
 
-import Header from "./header/Header.js";
+import Header from "./Header/Header.js";
+import Bio from "./Bio/Bio.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Bio />
     </div>
   );
 }
---
+++ src/Bio/Bio.js
@@ -0,0 +1,13 @@
+import './Bio.css';
+import '../fonts.css';
+
+function Bio(){
+  return(
+    <div className="bioBox">
+      <p className="robotoBold">Bio:</p>
+      <p className="robotoRegular">Lorem ipsum dolor sit amet. Ut molestiae repudiandae quo neque dicta a animi debitis a optio cumque qui modi dolores in quis sunt. Et praesentium recusandae in voluptates soluta qui Quis dolores et tenetur fugiat sit praesentium amet rem necessitatibus rerum. Rem praesentium iusto At enim enim ea impedit illo vel illum nemo ut nostrum quas.</p>
+    </div>
+  );
+}
+
+export default Bio;
