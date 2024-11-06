cc0c29f23a0a9d33e0c48fa3f60075af7461753f
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-06 23:03:00 +0000

    Add some bio text styling

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -4,8 +4,7 @@ import '../fonts.css';
 function Bio(){
   return(
     <div className="bioBox">
-      <p className="robotoBold">Bio:</p>
-      <p className="robotoRegular">Lorem ipsum dolor sit amet. Ut molestiae repudiandae quo neque dicta a animi debitis a optio cumque qui modi dolores in quis sunt. Et praesentium recusandae in voluptates soluta qui Quis dolores et tenetur fugiat sit praesentium amet rem necessitatibus rerum. Rem praesentium iusto At enim enim ea impedit illo vel illum nemo ut nostrum quas.</p>
+      <p className="robotoRegular bioText"><span className='robotoMedium bioTitle'>Bio:</span>Lorem ipsum dolor sit amet. Ut molestiae repudiandae quo neque dicta a animi debitis a optio cumque qui modi dolores in quis sunt. Et praesentium recusandae in voluptates soluta qui Quis dolores et tenetur fugiat sit praesentium amet rem necessitatibus rerum. Rem praesentium iusto At enim enim ea impedit illo vel illum nemo ut nostrum quas.</p>
     </div>
   );
 }
