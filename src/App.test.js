efb80431751ad350bfd067a14207837c14509990
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-07 22:54:34 +0000

    Remove unused test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders test message', () => {
-  render(<App />);
-  const title = screen.getByText(/George Jopson/i);
-  expect(title).toBeInTheDocument();
-});
