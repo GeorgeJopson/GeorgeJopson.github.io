f6dc3089a345760a1a5866fbee74254e4f6a42e1
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-10 19:22:44 +0000

    Close modal if clicked outside of

--- src/Modals/Modals.js
+++ src/Modals/Modals.js
@@ -4,8 +4,18 @@ function Modals({openModalIndex, modalsInfo, modalClose}) {
   const modals = modalsInfo.map((modalInfo,index)=>{
     let modalClass = (index === openModalIndex) ? "modalActive" : "modalHidden";
 
+    const handleModalClick = (event) => {
+      // Only close the modal if the click is outside of `.modal-content`
+      if (!event.target.closest('.modal-content')) {
+        modalClose();
+      }
+    };
+
     return(
-      <div className={"modal "+modalClass}>
+      <div 
+        className={"modal "+modalClass}
+        onClick={handleModalClick}
+      >
         <div class="modal-content">
           <p>We are at modal: {modalInfo.title}</p>
           <button onClick={modalClose}>Close</button>
