0e94872434cc313d45b7cff9e2313706e218a687
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-16 16:00:03 +0000

    Update Bio to include actual information

--- src/Bio/Bio.js
+++ src/Bio/Bio.js
@@ -4,7 +4,12 @@ import '../fonts.css';
 function Bio(){
   return(
     <div className="bioBox" id="bio">
-      <p className="robotoRegular bioText"><span className='robotoMedium bioTitle'>Bio:</span>Lorem ipsum dolor sit amet. Ut molestiae repudiandae quo neque dicta a animi debitis a optio cumque qui modi dolores in quis sunt. Et praesentium recusandae in voluptates soluta qui Quis dolores et tenetur fugiat sit praesentium amet rem necessitatibus rerum. Rem praesentium iusto At enim enim ea impedit illo vel illum nemo ut nostrum quas.</p>
+      <p className="robotoRegular bioText"><span className='robotoMedium bioTitle'>Bio:</span></p>
+      <ul>
+        <li className="robotoRegular bioBulletPoint"><span className='robotoMedium'>Junior Developer</span> - skilled in <span className="robotoMedium">Python</span>, <span className="robotoMedium">Java</span>, and <span className="robotoMedium">Web Development</span>.</li>
+        <li className="robotoRegular bioBulletPoint"><span className="robotoMedium">Software Developer Intern</span> at Wellcome Sanger Institute – <span className='robotoMedium'>1 year of hands-on experience.</span></li>
+        <li className="robotoRegular bioBulletPoint"><span className="robotoMedium">Computer Science Student</span> at the <span className="robotoMedium">University of York</span>.</li>
+      </ul>
     </div>
   );
 }
