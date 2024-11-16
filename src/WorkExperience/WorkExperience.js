fe5c04961a5c55d155ec9105eb15d3627bc6f6e3
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-16 18:26:49 +0000

    Added content to work experience section

--- src/WorkExperience/WorkExperience.js
+++ src/WorkExperience/WorkExperience.js
@@ -7,7 +7,15 @@ function WorkExperience(){
   return (
     <div className='containerBox' id="work-experience">
       <h1 className="robotoMedium">Work Experience:</h1>
-      <p className="robotoRegular">This is some text about work experience</p>
+
+      <h2 className="robotoMedium">Student Placment - Sanger Institute (2024-2025)</h2>
+      <p className="robotoRegular workExperienceDescription">
+        Worked for a year as a Student Placement Software Developer at the Wellcome Sanger Institute. 
+        The Wellcome Sanger Institute is a world class research centre in genomics, where 1000+ scientists, software developers, and support staff work
+        on cutting edge science.
+        <br/>
+        I was part of a team that built a CLI tool for tracking large-scale genomics metadata using Python and MongoDB, with a microservices architecture.
+      </p>
       <AchievementCarousel />
     </div>
 
