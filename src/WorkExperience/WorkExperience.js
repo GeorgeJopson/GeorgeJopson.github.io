f62a863a40a6956499719902fb7761a4b6f4ca51
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-11-16 16:08:37 +0000

    Fix bio and work experience titles

--- src/WorkExperience/WorkExperience.js
+++ src/WorkExperience/WorkExperience.js
@@ -6,6 +6,7 @@ import AchievementCarousel from '../AchievementCarousel/AchievementCarousel';
 function WorkExperience(){
   return (
     <div className='containerBox' id="work-experience">
+      <h1 className="robotoMedium">Work Experience:</h1>
       <p className="robotoRegular">This is some text about work experience</p>
       <AchievementCarousel />
     </div>
