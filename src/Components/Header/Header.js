b57a40af80d1cced3c6b1aa3a7a717286212beda
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-12-11 22:26:35 +0000

    Make title reveal in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,8 @@ import './Header.css';
 import '../../stylings/fonts.css';
 import '../../stylings/buttons.css';
 
+import { useState, useEffect } from "react";
+
 function Header(){
   return (
     <div className='header' id="header">
@@ -26,11 +28,29 @@ function ProfilePic(){
 }
 
 function HeaderTitleoBox(){
+  let titleIndex = 1;
+  const fullTitle = "George Jopson";
+  const [shownTitle, setShownTitle] = useState(fullTitle.slice(0,titleIndex));
+  const [hiddenTitle, setHiddenTitle] = useState(fullTitle.slice(titleIndex));
+
+  useEffect(() => {
+    const interval = setInterval(()=>{
+      setShownTitle(fullTitle.slice(0,titleIndex));
+      setHiddenTitle(fullTitle.slice(titleIndex));
+      titleIndex++;
+    }, 150);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [titleIndex]);
+
+
+
   return(
     <div className="titleBox">
         <h1 className="headerTitle robotoRegular">&gt;</h1>
         <div>
-          <h1 className='headerTitle robotoRegular'>George Jopson</h1>
+          <h1 className='headerTitle robotoRegular'>{shownTitle}<span className="hiddenText">{hiddenTitle}</span></h1>
           <p className="subtitle robotoRegular smallParagraphText">Junior Software Developer - georgejopson@gmail.com</p>
           <p className="subtitle robotoRegular smallParagraphText">Github: <a href="https://github.com/GeorgeJopson" className="subtitle robotoRegular" target="_blank" rel="noreferrer">https://github.com/GeorgeJopson</a></p>
           <DownloadCVButton />
