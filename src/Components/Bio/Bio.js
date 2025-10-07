f1109e9fccde5b80036973de505a5491cee7ec91
George Jopson <georgejopson@gmail.com>
2025-10-07 17:01:57 +0100

    Update Bio

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -9,7 +9,7 @@ function Bio(){
       <ul>
         <li className="robotoRegular paragraphText"><b>Junior Developer</b> - skilled in <b>Python</b>, <b>Java</b>, and <b>Web Development</b>.</li>
         <li className="robotoRegular paragraphText"><b>Software Developer Intern</b> at Wellcome Sanger Institute – <b>1 year of hands-on experience.</b></li>
-        <li className="robotoRegular paragraphText"><b>Computer Science Student</b> at the <b>University of York</b>.</li>
+        <li className="robotoRegular paragraphText"><b>Computer Science Student</b> at the <b>University of York</b> achieving <b>90%+ in 6 modules</b>.</li>
       </ul>
     </div>
   );
